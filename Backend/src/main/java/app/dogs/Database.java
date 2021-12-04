2231202add604e8bf619a229ca86abae3d78a098
Werner <guilhermeqwerner@gmail.com>
2021-12-04 14:45:10 -0300

    Add approved field to Testimonial class

--- Backend/src/main/java/app/dogs/Database.java
+++ Backend/src/main/java/app/dogs/Database.java
@@ -268,6 +268,7 @@ public class Database {
                 for (int i = 0; rs.next(); i++) {
                     testimonials[i] = new Testimonial(rs.getInt("id"), rs.getString("name"),
                             rs.getString("description"), rs.getInt("user_id"), rs.getString("type"),
+                            rs.getBoolean("type"),
                             rs.getString("location"), rs.getString("images"), rs.getString("recommendations"),
                             rs.getDate("date"));
                 }
@@ -290,7 +291,8 @@ public class Database {
 
             if (rs.first()) {
                 testimonial = new Testimonial(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
-                        rs.getInt("user_id"), rs.getString("type"), rs.getString("location"), rs.getString("images"),
+                        rs.getInt("user_id"), rs.getString("type"), rs.getBoolean("type"), rs.getString("location"),
+                        rs.getString("images"),
                         rs.getString("recommendations"),
                         rs.getDate("date"));
             }
