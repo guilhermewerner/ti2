195f46b55af7525b65d3d1e67bc5f90c0f7ac235
Werner <guilhermeqwerner@gmail.com>
2021-12-05 09:18:33 -0300

    Testimonial page funcionality

---
+++ Backend/src/main/java/app/dogs/Recomendations.java
@@ -0,0 +1,74 @@
+package app.dogs;
+
+public class Recomendations {
+    public static String[] abandonment() {
+        String[] array = new String[4];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Assumir a responsabilidade de ser um “lar temporário.";
+        array[3] = "Fazer um tratamento correto, com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] aggression() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] chaining() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] hygiene() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] environment() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] hungry() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+
+    public static String[] sick() {
+        String[] array = new String[3];
+
+        array[0] = "Ir à delegacia mais próxima para fazer uma denúncia.";
+        array[1] = "Passar o maior número de informações possíveis em relação ao infrator.";
+        array[2] = "Tratamento com acompanhamento veterinário.";
+
+        return array;
+    }
+}
